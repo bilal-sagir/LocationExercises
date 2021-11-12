38a7dbdab175423819cbb8b86accecd945d784e1
bilal-sagir <sagir.bilal@icloud.com>
2021-11-12 16:52:35 +0300

    set map

--- LocationExercises/ViewController.swift
+++ LocationExercises/ViewController.swift
@@ -1,13 +1,21 @@
 import UIKit
+import MapKit
 
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var myMap: MKMapView!
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        myMap.mapType = MKMapType.standard
+        myMap.isZoomEnabled = true
+        myMap.isScrollEnabled = true
+        myMap.isRotateEnabled = true
+        
     }
 
 
+    
 }
 
