dc5a24ece9fe4f58a733c06c979c6bb01c4eb298
bilal-sagir <sagir.bilal@icloud.com>
2021-11-12 18:11:28 +0300

    add Custom Annotation

--- LocationExercises/ViewController.swift
+++ LocationExercises/ViewController.swift
@@ -13,16 +13,36 @@ class ViewController: UIViewController
         myMap.isScrollEnabled = true
         myMap.isRotateEnabled = true
         let myCenter = CLLocationCoordinate2D(latitude: 40.978189, longitude: 29.041718)
+        
         let myRegion = MKCoordinateRegion.init(center: myCenter, latitudinalMeters: 300, longitudinalMeters: 300)
         myMap.setRegion(myRegion, animated: true)
         
-        let myAnnotation = MKPointAnnotation()
-        myAnnotation.coordinate = myCenter
-        myAnnotation.title = "Burası"
-        myAnnotation.subtitle = "subtitle yazi"
-        myMap.addAnnotation(myAnnotation)
+        let tommysCoor = CLLocationCoordinate2D(latitude: 40.978189, longitude: 29.042718)
         
+        let tommys = Restaurants(name: "Tommys", coord: tommysCoor, title: "Tommys", subtitle: "lol")
         
+        myMap.addAnnotation(tommys)
+        
+    }
+    
+    
+    class Restaurants : NSObject, MKAnnotation
+    {
+        var coordinate: CLLocationCoordinate2D
+        
+        var title: String?
+        var subtitle: String?
+        
+        var name: String
+        
+        init(name: String, coord: CLLocationCoordinate2D,
+             title:String?, subtitle: String? )
+        {
+            self.name = name
+            self.coordinate = coord
+            self.title = title
+            self.subtitle = subtitle
+        }
     }
 
 }
