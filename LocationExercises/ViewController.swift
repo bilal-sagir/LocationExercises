f13b1c73516bcb44c3061aeb230d36f68ad86fca
bilal-sagir <sagir.bilal@icloud.com>
2021-11-15 01:28:57 +0300

    see movement on the map

--- LocationExercises/ViewController.swift
+++ LocationExercises/ViewController.swift
@@ -1,8 +1,12 @@
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate
 {
+    @IBOutlet weak var map: MKMapView!
+    var curLoc : CLLocation!
+    
     var locManager: CLLocationManager!
     
     override func viewDidLoad()
@@ -12,9 +16,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate
         locManager = CLLocationManager()
         locManager.desiredAccuracy = kCLLocationAccuracyBest
         locManager.distanceFilter = 10 //in meters
-        
         locManager.delegate = self
-        
         locManager.startUpdatingLocation()
         locManager.requestAlwaysAuthorization()
         
@@ -23,7 +25,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager,
                          didUpdateLocations locations: [CLLocation])
     {
-        print(locations.last)
+        guard let loc = locations.last else {return}
+        
+        let locationAge = -loc.timestamp.timeIntervalSinceNow
+        print(loc.timestamp.timeIntervalSinceNow)
+        
+        if locationAge > 30.0
+        {
+            print("location update isnt happenning ")
+            return
+        }
+        
+        if self.curLoc == nil || self.curLoc.horizontalAccuracy >= loc.horizontalAccuracy
+        {
+            self.curLoc = loc
+            let zoomLocation = CLLocationCoordinate2D.init(latitude: curLoc.coordinate.latitude,
+                                                           longitude: curLoc.coordinate.longitude)
+            let newRegion = MKCoordinateRegion(center: zoomLocation,
+                                               latitudinalMeters: 300,
+                                               longitudinalMeters: 300)
+            map.setRegion(newRegion, animated: true)
+        }
+        
+        if loc.horizontalAccuracy <= self.locManager.desiredAccuracy
+        {
+            if CLLocationManager.locationServicesEnabled()
+            {
+                self.locManager.stopUpdatingLocation()
+            }
+        }
+        
     }
 
 
