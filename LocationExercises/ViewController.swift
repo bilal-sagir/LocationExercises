cea2d4f5d52dcc5788c507d10198abd283880870
bilal-sagir <sagir.bilal@icloud.com>
2021-11-15 00:43:17 +0300

    user location

--- LocationExercises/ViewController.swift
+++ LocationExercises/ViewController.swift
@@ -1,11 +1,29 @@
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
-
+    var locManager: CLLocationManager!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        locManager = CLLocationManager()
+        locManager.desiredAccuracy = kCLLocationAccuracyBest
+        locManager.distanceFilter = 10 //in meters
+        
+        locManager.delegate = self
+        
+        locManager.startUpdatingLocation()
+        locManager.requestAlwaysAuthorization()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation])
+    {
+        print(locations.last)
     }
 
 
